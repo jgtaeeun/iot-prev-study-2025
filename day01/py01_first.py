0e678c31883e2baddb669fc268dfd07849379474
jgtaeeun <jas49258667@gmail.com>
2025-01-15 17:21:48 +0900

    2일차 커밋

--- day01/py01_first.py
+++ day01/py01_first.py
@@ -1,3 +1,5 @@
-# iot과정 첫번째  프로그램파일
-if __name__ =='__main__':
-    print("Hello, Python!")
+# iot과정 첫번째  프로그램파일       -주석
+if __name__ =='__main__':           #조건문    / =(대입)  ==(동일)
+    print("Hello, Python!")         #출력함수 print
+
+    
